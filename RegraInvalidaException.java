a1581b093951e01e261bb7653fc835b7eef03721
Matheus Merechia Martins <matheusmerechia@gmail.com>
2025-10-28 23:26:45 -0300

    Add files via upload

---
+++ RegraInvalidaException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tp1.GestaoRH.dominio;
+
+/**
+ *
+ * @author matheusmerechia
+ */
+public class RegraInvalidaException extends RuntimeException {
+    public RegraInvalidaException(String message) {
+        super(message);
+    }
+}
