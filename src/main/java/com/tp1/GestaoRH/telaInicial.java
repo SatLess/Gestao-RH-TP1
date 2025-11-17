a68ee20abb29b892b84fbaadfd730c0cd44a5f61
Matheus Merechia Martins <matheusmerechia@gmail.com>
2025-11-17 00:35:28 -0300

    Add files via upload

--- src/main/java/com/tp1/GestaoRH/telaInicial.java
+++ src/main/java/com/tp1/GestaoRH/telaInicial.java
@@ -60,6 +60,7 @@ public class telaInicial extends JFrame {
             case "Administrador" -> {
                 new com.tp1.GestaoRH.ADMGUI().setVisible(true);
                 new com.tp1.GestaoRH.UI.MenuFinanceiro("Administrador").setVisible(true);
+                new com.tp1.GestaoRH.UI.TelaCadastroFuncionario("Administrador").setVisible(true);
             }
             case "GestorRH" -> {
                 new com.tp1.GestaoRH.UI.MenuFinanceiro("GestorRH").setVisible(true);
