b46dbb380891be747d69a4eb59aad093f129c6f1
Matheus Merechia Martins <matheusmerechia@gmail.com>
2025-11-17 01:26:54 -0300

    funcionario e .jar para pdf

--- src/main/java/com/tp1/GestaoRH/dominio/Funcionario.java
+++ src/main/java/com/tp1/GestaoRH/dominio/Funcionario.java
@@ -1,19 +1,25 @@
 package com.tp1.GestaoRH.dominio;
+import com.tp1.GestaoRH.Candidatura.*;
+import java.io.Serializable;
 
-public class Funcionario extends Pessoa {
+public class Funcionario extends Pessoa implements Serializable {
     private static final long serialVersionUID = 1L;
     private String cargo;
     private String tipoContratacao; // CLT, Estágio, PJ
     private double salarioBase;
     private boolean ativo;
+    private String departamento;
+    private String telefone;
 
-    public Funcionario(String nome, String email, String endereco, String cpf,
+    public Funcionario(String nome, String email, String endereco, String cpf, String departamento,String telefone,            
                        String cargo, double salarioBase, String tipoContratacao, boolean ativo) {
         super(nome, email, endereco, cpf);
         this.cargo = cargo;
+        this.telefone = telefone;
         this.salarioBase = salarioBase;
         this.tipoContratacao = tipoContratacao;
         this.ativo = ativo;
+        this.departamento = departamento;
     }
 
     public String getCargo() { return cargo; }
@@ -27,4 +33,10 @@ public class Funcionario extends Pessoa {
 
     public boolean isAtivo() { return ativo; }
     public void setAtivo(boolean ativo) { this.ativo = ativo; }
+    
+    public String getDepartamento() { return departamento;}
+    public void setDepartamento (String departamento) {this.departamento = departamento;}
+    
+    public String getTelefone() { return telefone;}
+    public void setTelefone (String telefone) {this.telefone = telefone;}
 }
